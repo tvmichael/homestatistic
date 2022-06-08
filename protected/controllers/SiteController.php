be0154ce9ce8853d758c34761e92d424a763b38e
tvmichael <michael.tmv@outlook.com>
2022-06-08 20:47:16 +0300

    controllers

---
+++ protected/controllers/SiteController.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\search\SpendingSearch;
+use Yii;
+use app\models\search\ProductSearch;
+use app\models\Spending;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        //Yii::$app->name = 'HomeStatistic';
+
+        return $this->render('index');
+    }
+
+    /**
+     * Displays statistic page.
+     *
+     * @return string
+     */
+    public function actionPurchase()
+    {
+        //Yii::$app->name = 'Purchase';
+
+        return $this->render('purchase', [
+            'productList' => ProductSearch::list(),
+            'spending' => SpendingSearch::today(),
+        ]);
+    }
+
+    /**
+     * Displays statistic page.
+     *
+     * @return string
+     */
+    public function actionPurchaseAddProductAjax()
+    {
+        $datetime = Yii::$app->request->post('datetime');
+        $productId = intval(Yii::$app->request->post('productId'));
+        $price = floatval(Yii::$app->request->post('price'));
+
+        $model = new Spending();
+        $model->productId = $productId;
+        $model->price = $price;
+        $model->datetime = date ('Y-m-d H:i:s', strtotime($datetime));
+
+        if($model->save()){
+            return 1;
+        }
+
+        return 0;
+    }
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return Response|string
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return string
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+}
